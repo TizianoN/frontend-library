3928401b36084dd85ed8cc276e2eb48d99e13671
TizianoN <hello@tizianonicolai.com>
2026-03-17 10:08:11 +0100

    add default template

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import BookList from "./pages/books/BookList";
 import BookDetail from "./pages/books/BookDetail";
+import DefaultTemplate from "./templates/DefaultTemplate";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* SITE ROUTES */}
-        <Route index Component={HomePage} />
+        <Route Component={DefaultTemplate}>
+          {/* SITE ROUTES */}
+          <Route index Component={HomePage} />
 
-        {/* BOOK ROUTES */}
-        <Route path="books">
-          <Route index Component={BookList} />
-          <Route path=":id" Component={BookDetail} />
+          {/* BOOK ROUTES */}
+          <Route path="books">
+            <Route index Component={BookList} />
+            <Route path=":id" Component={BookDetail} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/templates/DefaultTemplate.jsx
@@ -0,0 +1,44 @@
+import { NavLink, Outlet } from "react-router";
+
+export default function DefaultTemplate() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Frontend Library
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link" aria-current="page">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/books" className="nav-link" aria-current="page">
+                  Catalogo libri
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
