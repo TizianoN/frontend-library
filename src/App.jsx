0f012385c696d95754286a85c8bfb2eb32ca78d8
TizianoN <hello@tizianonicolai.com>
2026-03-20 12:55:57 +0100

    update app with context providers and create route

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import BookListPage from "./pages/books/BookListPage";
 import BookDetailPage from "./pages/books/BookDetailPage";
+import BookCreatePage from "./pages/books/BookCreatePage";
 import DefaultTemplate from "./templates/DefaultTemplate";
+import { LoaderContextProvider } from "./contexts/LoaderContext";
+import { NotificationContextProvider } from "./contexts/NotificationContext";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route Component={DefaultTemplate}>
-          {/* SITE ROUTES */}
-          <Route index Component={HomePage} />
+    <NotificationContextProvider>
+      <LoaderContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route Component={DefaultTemplate}>
+              {/* SITE ROUTES */}
+              <Route index Component={HomePage} />
 
-          {/* BOOK ROUTES */}
-          <Route path="books">
-            <Route index Component={BookListPage} />
-            <Route path=":id" Component={BookDetailPage} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+              {/* BOOK ROUTES */}
+              <Route path="books">
+                <Route index Component={BookListPage} />
+                <Route path=":id" Component={BookDetailPage} />
+                <Route path="create" Component={BookCreatePage} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </LoaderContextProvider>
+    </NotificationContextProvider>
   );
 }
