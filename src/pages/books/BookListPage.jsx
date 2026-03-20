867f672a95e5f53ac06362b23623357c25779488
TizianoN <hello@tizianonicolai.com>
2026-03-20 12:55:23 +0100

    update book list page with loader and create link

--- src/pages/books/BookListPage.jsx
+++ src/pages/books/BookListPage.jsx
@@ -1,21 +1,42 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import BookList from "../../components/books/BookList";
+import { Link } from "react-router";
+import { useLoaderContext } from "../../contexts/LoaderContext";
 
 export default function BooksIndex() {
   const [books, setBooks] = useState([]);
+  const { startLoading, endLoading } = useLoaderContext();
 
   useEffect(fetchBooks, []);
 
   function fetchBooks() {
-    axios.get("http://localhost:3000/books").then((res) => {
-      setBooks(res.data.result);
-    });
+    startLoading();
+
+    axios
+      .get("http://localhost:3000/books")
+      .then((res) => {
+        setBooks(res.data.result);
+      })
+      .catch((err) => {
+        showNotification(err.message, "danger");
+      })
+      .finally(() => {
+        endLoading();
+      });
   }
 
   return (
     <>
-      <h1>Catalogo libri</h1>
+      <div className="d-flex justify-content-between align-items-center mb-4">
+        <h1 className="m-0">Book list</h1>
+
+        <div className="d-flex gap-2 aling-items-center">
+          <Link className="btn btn-primary" to="/books/create">
+            Create book
+          </Link>
+        </div>
+      </div>
       <BookList books={books} />
     </>
   );
